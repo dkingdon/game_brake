38543a89e9cecd160c30e7d9d0d7972c2ebfc1e7
dkingdon <daniel.kingdon@gmail.com>
2016-12-22 12:28:07 -0800

    continued set up. Added express, mongoose, and body-parser. app working on localhost

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+// commenting out until needed
+// var mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/todo-app-demo')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
+
+// HTML endpoints
+app.get('/', function homepage (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+//Server listening on port 3000
+app.listen(process.env.PORT || 3000, function () {
+  console.log('server is running on http://localhost:3000/');
+});
