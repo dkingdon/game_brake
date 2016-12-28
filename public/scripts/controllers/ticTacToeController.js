78409431af3752ef42042dbf9af08c0130a0da03
dkingdon <daniel.kingdon@gmail.com>
2016-12-28 00:49:01 -0800

    tic tac toe logic refactored, added winner logic, initial computer player logic implemented.

--- public/scripts/controllers/ticTacToeController.js
+++ public/scripts/controllers/ticTacToeController.js
@@ -7,131 +7,143 @@ angular
   ticTacToeController.$inject = ['$http'];
   function ticTacToeController ($http) {
     var vm = this;
-    $(document).ready(function() {
-       // all code to manipulate the DOM
-       // goes inside this function
-       var targetId
-       var turnSymbol = 'O'
 
-       $('.col-md-4').click(function() {
-             /* Grabs unique square id and places it in the var targetId */
-         targetId = (this.id);
-         /* Decideds if the square is clickable via an id check */
-         if (targetId !== 'notAvail') {
-             /* Contitional statement that determines whos turn it is X or O */
-             if (turnSymbol === 'O'){
-                 turnSymbol = 'X';
-             }
-             else {
-                 turnSymbol = 'O';
-             }
-             /* Change square text value */
-             switch (targetId) {
-                 case 'topLeft':
-                     $('#topLeft').text(turnSymbol);
-                     $('#topLeft').prop('id', 'notAvail')
-                     break;
-                 case 'topMiddle':
-                     $('#topMiddle').text(turnSymbol);
-                     $('#topMiddle').prop('id', 'notAvail');
-                     break;
-                 case 'topRight':
-                     $('#topRight').text(turnSymbol);
-                     $('#topRight').prop('id', 'notAvail');
-                     break;
-                 case 'midLeft':
-                     $('#midLeft').text(turnSymbol);
-                     $('#midLeft').prop('id', 'notAvail');
-                     break;
-                 case 'midMiddle':
-                     $('#midMiddle').text(turnSymbol);
-                     $('#midMiddle').prop('id', 'notAvail');
-                     break;
-                 case 'midRight':
-                     $('#midRight').text(turnSymbol);
-                     $('#midRight').prop('id', 'notAvail');
-                     break;
-                 case 'botLeft':
-                     $('#botLeft').text(turnSymbol);
-                     $('#botLeft').prop('id', 'notAvail');
-                     break;
-                 case 'botMiddle':
-                     $('#botMiddle').text(turnSymbol);
-                     $('#botMiddle').prop('id', 'notAvail');
-                     break;
-                 case 'botRight':
-                     $('#botRight').text(turnSymbol);
-                     $('#botRight').prop('id', 'notAvail');
-                     break;
-                 default:
-                     break;
-                 }
-         }
-      });
-     });
+   var turnSymbol = 'O';
+   var tilesInPlay = [];
 
+   function checkAvailable(id){
+     for (var i = 0; i < tilesInPlay.length; i++) {
+       if (tilesInPlay[i] === id) {
+         return false
+       }
+     }
+     return true
+   }
+
+   // need logic for the 0-bot
+
+   $('.col-md-4').click(function() {
+     var targetId = (this.id);
+      var test = checkAvailable(targetId);
+      console.log(test);
+      // two players
+      if (checkAvailable(targetId)) {
+         turnSymbol === 'O' ? turnSymbol = 'X' : turnSymbol = 'O';
+         $('#' + targetId).text(turnSymbol);
+         tilesInPlay.push(targetId);
+         console.log(tilesInPlay); // NOTE remove before prod
+         setTimeout(checkWinner.bind(null, turnSymbol), 1000);
+         setTimeout(ohBot, 1000);
+
+      }
+    });
+
+     function checkWinner(turnSymbol) {
+       if ($('#topLeft').text() === turnSymbol & $('#topMiddle').text() === turnSymbol & $('#topRight').text() === turnSymbol) {
+         alert(turnSymbol + ' is the winner');
+       }
+       else if ($('#topLeft').text() === turnSymbol & $('#midLeft').text() === turnSymbol & $('#botLeft').text()  === turnSymbol){
+         alert(turnSymbol + ' is the winner');
+       }
+       else if ($('#topLeft').text() === turnSymbol & $('#midMiddle').text()  === turnSymbol & $('#botRight').text()  === turnSymbol){
+         alert(turnSymbol + ' is the winner');
+       }
+       else if ($('#topMiddle').text() === turnSymbol & $('#midMiddle').text() === turnSymbol & $('#botMiddle').text() === turnSymbol){
+         alert(turnSymbol + ' is the winner');
+       }
+       else if ($('#topRight').text() === turnSymbol & $('#midMiddle').text() === turnSymbol & $('#botLeft').text() === turnSymbol){
+         alert(turnSymbol + ' is the winner');
+       }
+       else if ($('#topRight').text() === turnSymbol & $('#midRight').text()  === turnSymbol & $('#botRight').text()  === turnSymbol){
+         alert(turnSymbol + ' is the winner');
+       }
+       else if ($('#midLeft').text() === turnSymbol & $('#midMiddle').text() === turnSymbol & $('#midRight').text()  === turnSymbol){
+         alert(turnSymbol + ' is the winner');
+       }
+       else if ($('#botLeft').text() === turnSymbol & $('#botMiddle').text() === turnSymbol & $('#botRight').text() === turnSymbol){
+         alert(turnSymbol + ' is the winner');
+       }
+     } // End of winner
+
+     var turns = 0;
+     function ohBot() {
+       var gameBoard = ['topLeft', 'topMiddle', 'topRight', 'midLeft', 'midMiddle', 'midRight', 'botLeft', 'botMiddle', 'botRight'];
+       if (turns === 0) {
+         var random = gameBoard[Math.floor(Math.random() * gameBoard.length)];
+         console.log(random)
+        var indexOf = gameBoard.indexOf(random)
+           if (random === 'X') {
+             gameBoard.spice(index, indexOf);
+             random = gameBoard[Math.floor(Math.random() * gameBoard.length)];
+             $('#' + random).trigger('click');
+             console.log(random)
+           }
+           else {
+             $('#' + random).trigger('click');
+             console.log(random)
+           }
+         turns += 1;
+       }
+       else if ($('#midMiddle').text() === 'X' & $('#topLeft').text() === 'X') {
+         $('#botRight').trigger('click');
+       }
+       else if ($('#midMiddle').text() === 'X' & $('#topMiddle').text() === 'X') {
+         $('#botMiddle').trigger('click');
+       }
+       else if ($('#midMiddle').text() === 'X' & $('#topRight').text() === 'X') {
+         $('#botLeft').trigger('click');
+       }
+       else if ($('#midMiddle').text() === 'X' & $('#midLeft').text() === 'X') {
+         $('#midRight').trigger('click');
+       }
+       else if ($('#midMiddle').text() === 'X' & $('#midRight').text() === 'X') {
+         $('#midLeft').trigger('click');
+       }
+       else if ($('#midMiddle').text() === 'X' & $('#botLeft').text() === 'X') {
+         $('#topRight').trigger('click');
+       }
+       else if ($('#midMiddle').text() === 'X' & $('#botMiddle').text() === 'X') {
+         $('#topMiddle').trigger('click');
+       }
+       else if ($('#midMiddle').text() === 'X' & $('#botRight').text() === 'X') {
+         $('#topLeft').trigger('click');
+       }
+       else if ($('#topLeft').text() === 'X' & $('#topMiddle').text() === 'X') {
+         $('#topRight').trigger('click');
+       }
+       else if ($('#topLeft').text() === 'X' & $('#topRight').text() === 'X') {
+         $('#topLeft').trigger('click');
+       }
+       else if ($('#topLeft').text() === 'X' & $('#midLeft').text() === 'X') {
+         $('#botLeft').trigger('click');
+       }
+       else if ($('#topRight').text() === 'X' & $('#midRight').text() === 'X') {
+         $('#botRight').trigger('click');
+       }
+       else if ($('#botLeft').text() === 'X' & $('#midLeft').text() === 'X') {
+         $('#topLeft').trigger('click');
+       }
+       else if ($('#botRight').text() === 'X' & $('#midRight').text() === 'X') {
+         $('#topRight').trigger('click');
+       }
+       else if ($('#topLeft').text() === 'X' & $('#botLeft').text() === 'X') {
+         $('#midLeft').trigger('click');
+       }
+       else if ($('#topLeft').text() === 'X' & $('#topRight').text() === 'X') {
+         $('#topMiddle').trigger('click');
+       }
+       else if ($('#topLeft').text() === 'X' & $('#botRight').text() === 'X') {
+         $('#midMiddle').trigger('click');
+       }
+       else if ($('#botLeft').text() === 'X' & $('#botRight').text() === 'X') {
+         $('#botMiddle').trigger('click');
+       }
+       else if ($('#botLeft').text() === 'X' & $('#topRight').text() === 'X') {
+         $('#midMiddle').trigger('click');
+       }
+       else if ($('#botRight').text() === 'X' & $('#topRight').text() === 'X') {
+         $('#midRight').trigger('click');
+       }
+     } // END of OBOT
 
   } //END OF CONTROLLER
-
-// $(document).ready(function() {
-//    // all code to manipulate the DOM
-//    // goes inside this function
-//    var targetId
-//    var turnSymbol = 'O'
-//
-//    $('.col-md-4').click(function() {
-//          /* Grabs unique square id and places it in the var targetId */
-//      targetId = (this.id);
-//      /* Decideds if the square is clickable via an id check */
-//      if (targetId !== 'notAvail') {
-//          /* Contitional statement that determines whos turn it is X or O */
-//          if (turnSymbol === 'O'){
-//              turnSymbol = 'X';
-//          }
-//          else {
-//              turnSymbol = 'O';
-//          }
-//          /* Change square text value */
-//          switch (targetId) {
-//              case 'topLeft':
-//                  $('#topLeft').text(turnSymbol);
-//                  $('#topLeft').prop('id', 'notAvail')
-//                  break;
-//              case 'topMiddle':
-//                  $('#topMiddle').text(turnSymbol);
-//                  $('#topMiddle').prop('id', 'notAvail');
-//                  break;
-//              case 'topRight':
-//                  $('#topRight').text(turnSymbol);
-//                  $('#topRight').prop('id', 'notAvail');
-//                  break;
-//              case 'midLeft':
-//                  $('#midLeft').text(turnSymbol);
-//                  $('#midLeft').prop('id', 'notAvail');
-//                  break;
-//              case 'midMiddle':
-//                  $('#midMiddle').text(turnSymbol);
-//                  $('#midMiddle').prop('id', 'notAvail');
-//                  break;
-//              case 'midRight':
-//                  $('#midRight').text(turnSymbol);
-//                  $('#midRight').prop('id', 'notAvail');
-//                  break;
-//              case 'botLeft':
-//                  $('#botLeft').text(turnSymbol);
-//                  $('#botLeft').prop('id', 'notAvail');
-//                  break;
-//              case 'botMiddle':
-//                  $('#botMiddle').text(turnSymbol);
-//                  $('#botMiddle').prop('id', 'notAvail');
-//                  break;
-//              case 'botRight':
-//                  $('#botRight').text(turnSymbol);
-//                  $('#botRight').prop('id', 'notAvail');
-//                  break;
-//              default:
-//                  break;
-//              }
-//      }
-//   });
-//  });
